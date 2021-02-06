63a0b2e347ccb4f61f7b5d6db92c8ecf8e27cbef
Krooter <cristian.voroneanu@isa.utm.md>
2021-02-06 22:03:13 +0200

    Add Generic Repository

---
+++ Core/Specification/ProductsSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace Core.Specification
+{
+    public class ProductsBrandTypeCategorySpecification : BaseSpecification<Product>
+    {
+        public ProductsBrandTypeCategorySpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+            AddInclude(x => x.ProductCategory);
+        }
+
+        public ProductsBrandTypeCategorySpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+            AddInclude(x => x.ProductCategory);
+        }
+    }
+}
